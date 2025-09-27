b9c3522a82ae7a32da6a8b0b876edce1395c7e2a
Eithan20 <eithan41g@gmail.com>
2025-09-26 20:35:26 -0400

    Tarea #1

---
+++ OperacionesMatematicas/src/com/operaciones/matematicas/OperacionesMatematicas.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.operaciones.matematicas;
+
+/**
+ *
+ * @author eitha
+ */
+/*
+// 2.Realizar un programa que incluya las 4 operaciones matemáticas básicas (Suma, Resta, Multiplicación y División).
+*/
+public class OperacionesMatematicas {
+   public static void main(String[] args) {
+       
+       //Declaramos las variables con valores enteros.
+        int valor1= 10;
+        int valor2= 5;
+        
+        //Operación de suma.
+        System.out.println(valor1+" + "+ valor2+" = "+(valor1+valor2));
+        
+        //Operación de resta.
+        System.out.println(valor1+" - "+ valor2+" = "+(valor1-valor2));
+         
+         //Operación de multiplicación,
+        System.out.println(valor1+" * "+ valor2+" = "+(valor1*valor2));
+         
+          //Operación de división,
+        System.out.println(valor1+" / "+ valor2+" = "+(valor1/valor2));
+         
+    }
+}
