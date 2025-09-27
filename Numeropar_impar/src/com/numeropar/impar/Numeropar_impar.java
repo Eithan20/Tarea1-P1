4868b4edc362a2c7eb62152e1a4f73a6d78f3d08
Eithan20 <eithan41g@gmail.com>
2025-09-26 20:33:41 -0400

    Tarea #1

---
+++ Numeropar_impar/src/com/numeropar/impar/Numeropar_impar.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.numeropar.impar;
+
+/**
+ *
+ * @author eitha
+ */
+
+//8. Realizar un programa que ingresado un numero, me indique si es par o impar.
+public class Numeropar_impar {
+    public static void main(String[] args) {
+       
+       //Inicializamos la variable num. 
+      int num =  28;
+      
+           //Verificamos si el numero es par o impar.
+        if (num % 2 == 0 ) {
+            System.out.println("El numero " + num + " es par.");
+        } else {
+            System.out.println("El numero " + num + " es impar");
+        }
+        
+        
+        
+        
+    }
+}
