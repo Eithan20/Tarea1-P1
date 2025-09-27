7ce2fceee79436c90b6f3a9a38389c573ebb5e28
Eithan20 <eithan41g@gmail.com>
2025-09-26 20:32:45 -0400

    Tarea #1

---
+++ DatosPersonales/src/DatosPersonales.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author eitha
+ */
+// 1. Realizar un programa que imprima por pantalla su nombre y correo electrónico.
+public class DatosPersonales {
+    public static void main(String[] args) {
+       
+       //Mostramos el nombre del usuario.
+        System.out.println("Eithan Perez");
+        
+        //Mostramos el correo del usuario..
+        System.out.println("eithan41g@gmail.com");
+    }
+       
+}
